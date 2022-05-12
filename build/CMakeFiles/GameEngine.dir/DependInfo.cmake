7f08b1fb572562575c2482ec6dc8ad5bb4993807
Crits <sale34@bk.ru>
2022-05-12 23:17:04 +0300

    first

---
+++ build/CMakeFiles/GameEngine.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/crits/programming/VSCode/gameEngine/src/main.cpp" "/home/crits/programming/VSCode/gameEngine/build/CMakeFiles/GameEngine.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../external/glfw/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/crits/programming/VSCode/gameEngine/build/external/glfw/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/external/glfw/src/CMakeFiles/glfw.dir/DependInfo.cmake
@@ -0,0 +1,51 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/crits/programming/VSCode/gameEngine/external/glfw/src/context.c" "/home/crits/programming/VSCode/gameEngine/build/external/glfw/src/CMakeFiles/glfw.dir/context.c.o"
+  "/home/crits/programming/VSCode/gameEngine/external/glfw/src/egl_context.c" "/home/crits/programming/VSCode/gameEngine/build/external/glfw/src/CMakeFiles/glfw.dir/egl_context.c.o"
+  "/home/crits/programming/VSCode/gameEngine/external/glfw/src/glx_context.c" "/home/crits/programming/VSCode/gameEngine/build/external/glfw/src/CMakeFiles/glfw.dir/glx_context.c.o"
+  "/home/crits/programming/VSCode/gameEngine/external/glfw/src/init.c" "/home/crits/programming/VSCode/gameEngine/build/external/glfw/src/CMakeFiles/glfw.dir/init.c.o"
+  "/home/crits/programming/VSCode/gameEngine/external/glfw/src/input.c" "/home/crits/programming/VSCode/gameEngine/build/external/glfw/src/CMakeFiles/glfw.dir/input.c.o"
+  "/home/crits/programming/VSCode/gameEngine/external/glfw/src/linux_joystick.c" "/home/crits/programming/VSCode/gameEngine/build/external/glfw/src/CMakeFiles/glfw.dir/linux_joystick.c.o"
+  "/home/crits/programming/VSCode/gameEngine/external/glfw/src/monitor.c" "/home/crits/programming/VSCode/gameEngine/build/external/glfw/src/CMakeFiles/glfw.dir/monitor.c.o"
+  "/home/crits/programming/VSCode/gameEngine/external/glfw/src/null_init.c" "/home/crits/programming/VSCode/gameEngine/build/external/glfw/src/CMakeFiles/glfw.dir/null_init.c.o"
+  "/home/crits/programming/VSCode/gameEngine/external/glfw/src/null_joystick.c" "/home/crits/programming/VSCode/gameEngine/build/external/glfw/src/CMakeFiles/glfw.dir/null_joystick.c.o"
+  "/home/crits/programming/VSCode/gameEngine/external/glfw/src/null_monitor.c" "/home/crits/programming/VSCode/gameEngine/build/external/glfw/src/CMakeFiles/glfw.dir/null_monitor.c.o"
+  "/home/crits/programming/VSCode/gameEngine/external/glfw/src/null_window.c" "/home/crits/programming/VSCode/gameEngine/build/external/glfw/src/CMakeFiles/glfw.dir/null_window.c.o"
+  "/home/crits/programming/VSCode/gameEngine/external/glfw/src/osmesa_context.c" "/home/crits/programming/VSCode/gameEngine/build/external/glfw/src/CMakeFiles/glfw.dir/osmesa_context.c.o"
+  "/home/crits/programming/VSCode/gameEngine/external/glfw/src/platform.c" "/home/crits/programming/VSCode/gameEngine/build/external/glfw/src/CMakeFiles/glfw.dir/platform.c.o"
+  "/home/crits/programming/VSCode/gameEngine/external/glfw/src/posix_module.c" "/home/crits/programming/VSCode/gameEngine/build/external/glfw/src/CMakeFiles/glfw.dir/posix_module.c.o"
+  "/home/crits/programming/VSCode/gameEngine/external/glfw/src/posix_poll.c" "/home/crits/programming/VSCode/gameEngine/build/external/glfw/src/CMakeFiles/glfw.dir/posix_poll.c.o"
+  "/home/crits/programming/VSCode/gameEngine/external/glfw/src/posix_thread.c" "/home/crits/programming/VSCode/gameEngine/build/external/glfw/src/CMakeFiles/glfw.dir/posix_thread.c.o"
+  "/home/crits/programming/VSCode/gameEngine/external/glfw/src/posix_time.c" "/home/crits/programming/VSCode/gameEngine/build/external/glfw/src/CMakeFiles/glfw.dir/posix_time.c.o"
+  "/home/crits/programming/VSCode/gameEngine/external/glfw/src/vulkan.c" "/home/crits/programming/VSCode/gameEngine/build/external/glfw/src/CMakeFiles/glfw.dir/vulkan.c.o"
+  "/home/crits/programming/VSCode/gameEngine/external/glfw/src/window.c" "/home/crits/programming/VSCode/gameEngine/build/external/glfw/src/CMakeFiles/glfw.dir/window.c.o"
+  "/home/crits/programming/VSCode/gameEngine/external/glfw/src/x11_init.c" "/home/crits/programming/VSCode/gameEngine/build/external/glfw/src/CMakeFiles/glfw.dir/x11_init.c.o"
+  "/home/crits/programming/VSCode/gameEngine/external/glfw/src/x11_monitor.c" "/home/crits/programming/VSCode/gameEngine/build/external/glfw/src/CMakeFiles/glfw.dir/x11_monitor.c.o"
+  "/home/crits/programming/VSCode/gameEngine/external/glfw/src/x11_window.c" "/home/crits/programming/VSCode/gameEngine/build/external/glfw/src/CMakeFiles/glfw.dir/x11_window.c.o"
+  "/home/crits/programming/VSCode/gameEngine/external/glfw/src/xkb_unicode.c" "/home/crits/programming/VSCode/gameEngine/build/external/glfw/src/CMakeFiles/glfw.dir/xkb_unicode.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "_DEFAULT_SOURCE"
+  "_GLFW_X11"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../external/glfw/include"
+  "../external/glfw/src"
+  "external/glfw/src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
